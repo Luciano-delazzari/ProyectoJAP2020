fa9a1afec406d3743391b084a1705bb7ba535348
Luciano-delazzari <ldelazzari17@gmail.com>
2020-08-11 20:46:24 -0300

    Login hecho
    falta alerta preguntar en consulta

--- js/login.js
+++ js/login.js
@@ -2,5 +2,27 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("boton").addEventListener("click", function(e) {
+        let email = document.getElementById("inputEmail");
+        let password = document.getElementById("inputPassword");
+        let completo = true;
+
+        if (email.value === "") {
+            email.classList.add("error");
+            completo == false;
+        }
+
+        if (password.value === "") {
+            password.classList.add("error")
+            completo == false;
+        }
+
+        if(completo) {
+           window.location = "index.html"
+        } else{
+            alert("Debe completar!");
+        }
+
+    });
 
 });
