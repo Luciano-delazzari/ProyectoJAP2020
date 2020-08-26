0a4fad52075275aa6d204e02277d21d62fe06c74
Luciano-delazzari <ldelazzari17@gmail.com>
2020-08-26 18:43:58 -0300

    filtro de precios

--- js/products.js
+++ js/products.js
@@ -1,4 +1,6 @@
 var arrayDeProductos = [];
+var min = undefined;
+var max = undefined;
 
 function mostrarProductos(array) {
   let contenidoaAgregar =  "";
@@ -6,25 +8,28 @@ function mostrarProductos(array) {
   for (let index = 0; index < array.length; index++) {
       products = array[index];
 
-      contenidoaAgregar += ` 
-      <div class="row">
-          <div class="col-3">
-              <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-          </div>
-          <hr>
-          <div class="col">
-              <div class="d-flex w-100 justify-content-between">
-                  <h4 class="mb-1">`+ products.name +`</h4>
-                  <small class="text-muted">` + products.soldCount + ` artículos</small>
-              </div>
-              <hr>
-              <p class="mb-1">` + products.description + `</p>
-              <hr>
-          </div>
-          <hr>
-      </div>
-      <hr>
-  `
+      if           (((min == undefined) || (min != undefined && parseInt(category.productCount) >= min)) &&
+                   ((max == undefined) || (max != undefined && parseInt(category.productCount) <= max))){
+
+               contenidoaAgregar += ` 
+                    <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name +`</h4>
+                            <small class="text-muted">` + products.cost + ` USD</small>
+                        </div>
+                        <p class="mb-1">` + products.description + `</p>
+                    </div>
+                </div>
+            </a>
+            <hr>
+            `
+        }
+  
       
       
    
@@ -32,12 +37,6 @@ function mostrarProductos(array) {
       document.getElementById("contenido").innerHTML = contenidoaAgregar
   }
 }
-
-
-
-
-
-
 
 document.addEventListener("DOMContentLoaded", function(e){
     
@@ -49,6 +48,41 @@ document.addEventListener("DOMContentLoaded", function(e){
             mostrarProductos(arrayDeProductos);
         }
     });
+
+   document.getElementById("botonfiltro").addEventListener("click" , function () {
+      
+        min = document.getElementById("minprecio").value;
+        max = document.getElementById("maxprecio").value;
+
+        if((min != undefined) && (min != "") && (parseInt(min)) >= 0) {
+            min = parseInt(min);
+        } else {
+            min = undefined
+        }
+
+        if((max != undefined) && (min != "") && (parseInt(max)) >= 0) {
+            max = parseInt(max);
+        } else {
+            max = undefined
+        }
+
+        mostrarProductos();
+
+   });
+    document.getElementById("botonlimpiar").addEventListener("click" , function () {
+
+document.getElementById("minprecio").value = "";
+document.getElementById("maxprecio").value = "";
+
+min = undefined;
+max = undefined;
+
+
+mostrarProductos();
+
+    });
+
+
 });
 
 
